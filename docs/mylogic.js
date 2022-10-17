9c737530a8ace509901cee02171c99c7abdaff20
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2022-10-17 18:37:06 -0400

    page functioning, adding comments

--- docs/mylogic.js
+++ docs/mylogic.js
@@ -1,5 +1,4 @@
 //Render today's date into header
-
 function todaysDate() {
     var tDate = moment().local().format("ddd, MMM Do, YYYY - h:mm:ss a");
     $("#currentDay").text(tDate);
@@ -8,39 +7,14 @@ function todaysDate() {
     //console.log(tDate);
 }
 
-var nineam = document.getElementById("hour9");
-var tenam = document.getElementById("hour10");
-var elevenam = document.getElementById("hour11");
-var twelvepm = document.getElementById("hour12");
-var onepm = document.getElementById("hour13");
-var twopm = document.getElementById("hour14");
-var threepm = document.getElementById("hour15");
-var fourpm = document.getElementById("hour16");
-var fivepm = document.getElementById("hour17");
-
-var nineamrow = document.getElementById("hour-9");
-var tenamrow = document.getElementById("hour-10");
-var elevenamrow = document.getElementById("hour-11");
-var twelvepmrow = document.getElementById("hour-12");
-var onepmrow = document.getElementById("hour-13");
-var twopmrow = document.getElementById("hour-14");
-var threepmrow = document.getElementById("hour-15");
-var fourpmrow = document.getElementById("hour-16");
-var fivepmrow = document.getElementById("hour-17");
-
 function changeColor() {
-    currentTime = parseInt(moment().local().format("H"));
-    console.log(currentTime);
+    var currentTime = parseInt(moment().local().format("H"));
     var rowTime = document.getElementsByClassName("number");
-    console.log(rowTime);
     var rowTimeArray = Object.values(rowTime);
-    console.log(rowTimeArray);
     var rowDiv = document.getElementsByClassName("col-md-10 description")
     var rowDivArray = Object.values(rowDiv);
     for (i = 0; i < rowDivArray.length; i++) {
     var rowTimeArrayText = +rowTimeArray[i].outerText;
-    console.log(typeof rowTimeArrayText);
-    console.log(typeof currentTime);
     if (rowTimeArrayText === currentTime) {
         rowDiv[i].classList.add("present");
     } else if (rowTimeArrayText < currentTime) {
@@ -51,40 +25,54 @@ function changeColor() {
 }
 }
 
+function sendToLocal() {
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })})
+}
+
+function getFromLocal() {
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+}
+
+var currentTime = parseInt(moment().local().format("H"));
+var rowTime = document.getElementsByClassName("number");
+var rowTimeArray = Object.values(rowTime);
+var rowTimeArrayNumber = rowTimeArray[5].outerText;
+function changeToTwelve() {
+    $("#hour-13 .number").text(rowTimeArray[4].outerText - 12);
+    $("#hour-14 .number").text(rowTimeArray[5].outerText - 12);
+    $("#hour-15 .number").text(rowTimeArray[6].outerText - 12);
+    $("#hour-16 .number").text(rowTimeArray[7].outerText - 12);
+    $("#hour-17 .number").text(rowTimeArray[8].outerText - 12);
+    $("#hour-9 .ampm").text("am");
+    $("#hour-10 .ampm").text("am");
+    $("#hour-11 .ampm").text("am");
+    $("#hour-12 .ampm").text("pm");
+    $("#hour-13 .ampm").text("pm");
+    $("#hour-14 .ampm").text("pm");
+    $("#hour-15 .ampm").text("pm");
+    $("#hour-16 .ampm").text("pm");
+    $("#hour-17 .ampm").text("pm");
+}
+
 //Add onload events here
 window.onload = function onload() {
     todaysDate();
     $("#notify").hide();
     changeColor();
+    getFromLocal();
+    sendToLocal();
+    changeToTwelve();
 }
-
-/*
-var saveButtons = document
-$(document).ready(function () {
-    // listen for save button clicks
-    $('.saveBtn').on('click', function () {
-        // get nearby values
-
-        // save in localStorage
-
-        // Show notification that item was saved to localStorage by adding class 'show'
-
-        // Timeout to remove 'show' class after 2 seconds
-        setTimeout(function () {
-            $('.notification').removeClass('show');
-        }, 2000);
-    });
-*/
-/*
-document.getElementsByClassName("btn").addEventListener("click", function saveToLocal() {
-    var toDoNotes = document.getElementsByClassName("description").value;
-    localStorage.setItem("notes", toDoNotes);
-    $("#notify").show();
-    console.log("To-Do List Saved")
-}
-)
-*/
-
-
-
-
